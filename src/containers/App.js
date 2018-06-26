93e0ee1694c3f5a27695d77596e7bd8f7d20100a
V4NC3 <36180645+V4NC3@users.noreply.github.com>
2018-06-25 22:44:15 -0700

    v1

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import logo from '../images/logo.svg';
+//import './App.css';
+import '../containers/App.css';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import './index.css';
+import './containers/index.css';
+//import App from './App';
+import App from './containers/App';
+//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
