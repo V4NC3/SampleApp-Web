4bf4e4efbb0b359e5ba5afaac221b12976a9c11c
V4NC3 <36180645+V4NC3@users.noreply.github.com>
2018-07-10 01:59:38 -0700

    updates to routes

---
+++ src/Routes.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Login from './components/client/Login';
+import Logout from './components/client/Logout';
+import Registration from './components/client/Registration';
+
+//Routes
+const Routes = () => {
+    <Router>
+        {/* navi bar */}
+        <div className='nav'>
+            <ul>
+                <li>
+                    <Link to="/">Home </Link>
+                </li>
+                <li>
+                    <Link to="/about">About </Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/logout">Logout</Link>
+                </li>
+                <li>
+                    <Link to="/registration">Registration </Link>
+                </li>
+            </ul>
+
+            <hr />
+
+            {/* routes */}
+            <Route exact path='/' component={Home} />
+            <Route path='about' component={About} />
+            <Route path='login' component={Login} />
+            <Route path='logout' component={Logout} />
+            <Route path='registration' component={Registration} />
+
+        </div>
+    </Router>
+};
+
+//React codes
+
+export default Routes;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
-import '../containers/App.css';
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React Home Page</h1>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+                </p>
+
+                <Home />
             </div>
         );
     }
