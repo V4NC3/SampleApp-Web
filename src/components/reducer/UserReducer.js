2888fd575a74594863c6052c414bfc39067138c1
VAN NGUYEN <nguyen.vance@gmail.com>
2018-07-16 23:46:50 -0700

    login

---
+++ src/components/reducer/UserReducer.js
@@ -0,0 +1,40 @@
+const UserReducer = (
+    state = {
+        //...state,
+        isLoggedIn: false
+        , userName: ''
+        , firstName: ''
+        , lastName: ''
+
+
+    }, action) => {
+    switch (action.type) {
+        case 'SET_LOGIN_STATE_FULFILLED':
+            state = {
+                ...state,
+                isLoggedIn: true
+                , userName: ''
+            };
+            break;
+        // default: state = {
+        //     isLoggedIn: false
+        // };
+        case 'SET_HOMEPAGE_LOGIN_STATE':
+            state = {
+                ...state,
+                userName: ''
+                , firstName: action.payload.firstName,
+                lastName: action.payload.lastName,
+            };
+            break;
+        default:
+            // state = {
+            //     ...state,
+            //     userName: ''
+            //     , isLoggedIn: false
+            // };
+            break;
+    }
+    return state;
+};
+export default UserReducer;
